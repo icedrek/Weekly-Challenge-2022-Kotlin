379efa1fdadc1e0a26cc3633d2117e91fabd17b7
Gabi <icedrek@gmail.com>
2022-11-28 20:36:12 +0100

    Challenge47_Kotlin

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge47.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge47.kt
@@ -17,3 +17,36 @@ package com.mouredev.weeklychallenge2022
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+ fun main() {
+    countVocals("Hola mundo KOTLIN")
+}
+
+fun countVocals(input: String = "") {
+    if (input.isEmpty()) return println("No hay texto que comprobar")
+    
+    val letters = listOf("a", "e", "i", "o", "u")
+    val counters = mutableListOf(0, 0, 0, 0, 0)
+    
+    var valueMax = 0
+
+    input.forEach { inputChar ->
+        for (letter in letters.indices) {            
+            if (letters[letter] == inputChar.toString().lowercase()) counters[letter] += 1      
+            
+            if (counters[letter] > valueMax) valueMax = counters[letter]
+        }
+    }
+    
+    if (valueMax <= 0) return println ("Que vocales, ni vocales... Aqui no hay de eso")        
+    
+    if (valueMax > 0) {
+        print("Las letras ")
+        
+        for (letter in counters.indices) 
+            if (counters[letter] == valueMax) print("'${letters[letter]}', ")               
+        
+        println("aparecen $valueMax veces")         
+    } 
+    
+    for (letter in letters.indices) println("${letters[letter]}: ${counters[letter]}")
+}
